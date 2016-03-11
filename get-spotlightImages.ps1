c476affc9ebcd29d4d0f5d07e0fe7576da6594c0
Oli Newsham <oli.newsham@slicedbread.co.uk>
2016-03-11 13:13:07 +0000

    Initial commit

---
+++ get-spotlightImages.ps1
@@ -0,0 +1,16 @@
+﻿$imagesFolder = [Environment]::GetFolderPath("LocalApplicationData") + "\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
+$outputPath = [Environment]::GetFolderPath("MyPictures") + "\Spotlight Images";
+
+$File = Get-ChildItem -Path $imagesFolder -Recurse
+
+if(!(Test-Path -Path $outputPath )){
+    New-Item -ItemType directory -Path $outputPath
+}
+
+for($i = 0; $i -lt $File.Count; $i++)
+{
+   if ($File[$i].Length -gt 250000)
+   {
+    Copy-Item $File[$i].FullName ($outputPath + "\" + $File[$i].BaseName + $i +".jpg");
+   }
+}
